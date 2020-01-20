2a6500d6cab41e709b8314f1fff90b26c0813833
Joseph Kempster <jkempster34@gmail.com>
2020-01-20 12:25:53 +0000

    Refactor petrol tax solution to use switch statement.

--- exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
@@ -10,61 +10,59 @@ namespace TaxCalculator.Tests
         public override int CalculateTax(Vehicle vehicle)
         {
             int vehicleCo2Emissions = vehicle.Co2Emissions;
+            int? firstTaxPayment = null;
 
-            if (vehicleCo2Emissions == 0)
+            switch (vehicleCo2Emissions)
             {
-                return 0;
-            }
-            else if (vehicleCo2Emissions < 51)
-            {
-                return 10;
-            }
-            else if (vehicleCo2Emissions < 76)
-            {
-                return 25;
-            }
-            else if (vehicleCo2Emissions < 91)
-            {
-                return 105;
-            }
-            else if (vehicleCo2Emissions < 101)
-            {
-                return 125;
-            }
-            else if (vehicleCo2Emissions < 111)
-            {
-                return 145;
-            }
-            else if (vehicleCo2Emissions < 131)
-            {
-                return 165;
-            }
-            else if (vehicleCo2Emissions < 151)
-            {
-                return 205;
-            }
-            else if (vehicleCo2Emissions < 171)
-            {
-                return 515;
-            }
-            else if (vehicleCo2Emissions < 191)
-            {
-                return 830;
-            }
-            else if (vehicleCo2Emissions < 226)
-            {
-                return 1240;
-            }
-            else if (vehicleCo2Emissions < 256)
-            {
-                return 1760;
-            }
-            else if (vehicleCo2Emissions >= 256)
-            {
-                return 2070;
+                case 0:
+                    firstTaxPayment = 0;
+                    break;
+                case int n when (n < 51 && n >= 1):
+                    firstTaxPayment = 10;
+                    break;
+                case int n when (n < 76 && n >= 51):
+                    firstTaxPayment = 25;
+                    break;
+                case int n when (n < 91 && n >= 76):
+                    firstTaxPayment = 105;
+                    break;
+                case int n when (n < 101 && n >= 91):
+                    firstTaxPayment = 125;
+                    break;
+                case int n when (n < 111 && n >= 101):
+                    firstTaxPayment = 145;
+                    break;
+                case int n when (n < 131 && n >= 111):
+                    firstTaxPayment = 165;
+                    break;
+                case int n when (n < 151 && n >= 131):
+                    firstTaxPayment = 205;
+                    break;
+                case int n when (n < 171 && n >= 151):
+                    firstTaxPayment = 515;
+                    break;
+                case int n when (n < 191 && n >= 171):
+                    firstTaxPayment = 830;
+                    break;
+                case int n when (n < 226 && n >= 191):
+                    firstTaxPayment = 1240;
+                    break;
+                case int n when (n < 256 && n >= 226):
+                    firstTaxPayment = 1760;
+                    break;
+                case int n when (n >= 256):
+                    firstTaxPayment = 2070;
+                    break;
             }
 
-            return 0;
+            if (firstTaxPayment == null)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                return firstTaxPayment.Value;
+            }
         }
     }
 }
