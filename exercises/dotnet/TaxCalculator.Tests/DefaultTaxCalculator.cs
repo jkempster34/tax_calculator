f321a71563d6408f162452e3aa56587df34390de
Joseph Kempster <jkempster34@gmail.com>
2020-01-21 12:01:25 +0000

    Add solution for story 4

--- exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
@@ -6,11 +6,38 @@ namespace TaxCalculator.Tests
 {
     public class DefaultTaxCalculator : TaxCalculator
     {
+        private bool ToggleFeatureAfterFirstYear { get; set; }
+
+        public DefaultTaxCalculator()
+        {
+        }
+
+        public DefaultTaxCalculator(bool toggleFeatureAfterFirstYear)
+        {
+            ToggleFeatureAfterFirstYear = toggleFeatureAfterFirstYear;
+        }
+
         public override int CalculateTax(Vehicle vehicle)
         {
             var emissions = vehicle.Co2Emissions;
             int? cost = null;
             Dictionary<int, int> index = null;
+
+            if (ToggleFeatureAfterFirstYear)
+            {
+                if(DateTime.Now.AddYears(-1).Year >= vehicle.DateOfFirstRegistration.Year)
+                {
+                    switch (vehicle.FuelType)
+                    {
+                        case FuelType.Petrol:
+                            return 140;
+                        case FuelType.Diesel:
+                            return 140;
+                        case FuelType.AlternativeFuel:
+                            return 130;
+                    }
+                }
+            }
 
             switch (vehicle.FuelType)
             {
